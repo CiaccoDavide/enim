4591d247fd39689bfbc51eba0dec33a172a677e9
CiaccoDavide <davideciacco@CDMB.local>
2015-06-24 15:25:11 +0200

    rephpd

--- autopvp/getUserGear.php
+++ autopvp/getUserGear.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	require_once './dbconnect.php';
 
@@ -10,17 +9,4 @@
 	echo $row['gear'];
 
 	header('Content-type: application/json');
-=======
-<?php
-	require_once './dbconnect.php';
-
-	$username=$_GET['username'];
-
-	$sql="SELECT gear FROM gears WHERE username='$username'";
-	$result=mysqli_query($db, $sql);
-	$row=mysqli_fetch_array($result);
-	echo $row['gear'];
-
-	header('Content-type: application/json');
->>>>>>> origin/master
 ?>
