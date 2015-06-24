713caf00afa5e6be10e9f2cf6adee31a157f0f5c
CiaccoDavide <davideciacco@CDMB.local>
2015-06-24 15:18:45 +0200

    Revert 6268772..aa1046c
    This rolls back to commit 6268772aba81ec566ea6d8a3dd01f72c16fae45b.

--- autopvp/dbconnect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$db=mysqli_connect('localhost','admin','pwd','autopvp');
-	if(mysqli_connect_errno()) echo "Failed to connect to MySQL: ".mysqli_connect_error();
-	date_default_timezone_set('Europe/Rome');
-?>
