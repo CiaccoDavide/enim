02ed33e492010c2e711bed24a3e7c429abfa1d2e
CiaccoDavide <davideciacco@CDMB.local>
2015-06-24 15:23:15 +0200

    Revert ae97851..713caf0
    This rolls back to commit ae97851f6d8530060bc8c8ba68b19f51235fea9a.

---
+++ autopvp/getUserGear.php
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+<?php
+	require_once './dbconnect.php';
+
+	$username=$_GET['username'];
+
+	$sql="SELECT gear FROM gears WHERE username='$username'";
+	$result=mysqli_query($db, $sql);
+	$row=mysqli_fetch_array($result);
+	echo $row['gear'];
+
+	header('Content-type: application/json');
+=======
+<?php
+	require_once './dbconnect.php';
+
+	$username=$_GET['username'];
+
+	$sql="SELECT gear FROM gears WHERE username='$username'";
+	$result=mysqli_query($db, $sql);
+	$row=mysqli_fetch_array($result);
+	echo $row['gear'];
+
+	header('Content-type: application/json');
+>>>>>>> origin/master
+?>
---
+++ autopvp/getUserInv.php
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+<?php
+	require_once './dbconnect.php';
+
+	$username=$_GET['username'];
+
+	$sql="SELECT inventory FROM inventories WHERE username='$username'";
+	$result=mysqli_query($db, $sql);
+	$row=mysqli_fetch_array($result);
+	echo $row['inventory'];
+
+	header('Content-type: application/json');
+=======
+<?php
+	require_once './dbconnect.php';
+
+	$username=$_GET['username'];
+
+	$sql="SELECT inventory FROM inventories WHERE username='$username'";
+	$result=mysqli_query($db, $sql);
+	$row=mysqli_fetch_array($result);
+	echo $row['inventory'];
+
+	header('Content-type: application/json');
+>>>>>>> origin/master
+?>
